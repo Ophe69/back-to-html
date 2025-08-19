9a3c724af7a43069b6c467c7a5ecaa6b92d876f2
OpheliaTL <ophelia.tagbo@gmail.com>
2025-08-19 20:02:14 +0200

    adding card

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+// ===== Date du jour =====
+const dateArea = document.getElementById("dateArea")
+const today = new Date()
+dateArea.textContent = today.toLocaleDateString("fr-FR", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+})
+
+// ===== Formulaire =====
 const form = document.getElementById("contactForm")
 const response = document.getElementById("formResponse")
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault() // Empêche le rechargement de la page
+  e.preventDefault()
   response.textContent =
     "Merci " +
     document.getElementById("name").value +
-    " ! Votre message a bien été envoyé ✅"
+    " ! Votre message a été envoyé ✅"
   response.style.color = "green"
   form.reset()
 })
+
+// ===== Bouton Hero =====
+const ctaButton = document.getElementById("ctaButton")
+ctaButton.addEventListener("click", () => {
+  alert("Merci d'avoir cliqué 🎉")
+})
+
+// ===== Footer année dynamique =====
+document.getElementById("year").textContent = new Date().getFullYear()
